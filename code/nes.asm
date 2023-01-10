b7e7678f320146943e1ed4137f6435ee3c299d99
Floortje Tjeertes <43434642+floriantjeertes@users.noreply.github.com>
2023-01-10 17:36:25 +0100

    background subroutine made

--- code/nes.asm
+++ code/nes.asm
@@ -71,6 +71,8 @@ LOADPALETTES:
   cpx #$20
   bne LOADPALETTES
 
+
+
   ldx #$00
   
 LOADSPRITES:
@@ -83,55 +85,158 @@ LOADSPRITES:
 
 LOADBACKGROUND:
 
-  lda #$20
-  sta $2006 
-  lda #$00
-  sta $2006 
+  lda #$20    ;load 20 into acumulator
+  sta $2006   ;stores most significant byte in ppu
+  lda #$00   ;stores least sugnificant byte in acumulator 
+  sta $2006   ;stores least significant byte in ppu
 
-  ldx #$00
+  ldx #$00   ;load x with 0
 
 :
-  txa 
-  sta $2007
+  txa       ;transfer x to acumulator
+  sta $2007 ;stores acumulator in ppu address 2007
   inx
-  cpx #$FF
+  cpx #$CE ;compare x with 255
   bne :-
 
-  
 
 ;enable interupts
   cli
 
-  lda #%10010000 ;enable nmi change background to use second char set
+  lda #%10000000 ;enable nmi change background to use second char set
   sta $2000  ;PPUCTRL ppu controll register
   lda #%00011110   ;enable sprites and backgrounds for left most 8 pixels
   sta $2001
 
+
+
+
+;
 ;program loop
+
+
+  jsr DISPLAY_BACKGROUND
+
+
+
+ldx #$00
+ldy #$00
+
 LOOP:
-lda $00
-sta $1800
+ jsr WAITVBLANK
+txa
+sta $0200
+sta $0204
+adc #$08
+sta $0208
+adc #$08
+sta $020c
 
+tya
+sta $0203
+adc #$08
+sta $0207
+sbc #$07
+sta $020B
+adc #$08
+sta $020F
 
-lda $01
-sta $1800
-jsr do_action
+iny
+inx
 
 jmp LOOP
 
-;test loop
-Loopy:
-nop
+
+DISPLAY_BACKGROUND:
+
+
+  lda #%00000000   ;enable sprites and backgrounds for left most 8 pixels
+  sta $2001
+  sei
+
+  lda #$28
+  sta $2006 ;store most significant value 3f in ppu write address 3f.. (the adress where you store the address you want to write too in the ppu)
+  lda #$C0
+  sta $2006 ;store least significant value 00 in ppu write address ..00
+
+  ldx #$00
+  
+
+  lda #$20    ; load the value 32 into the accumulator
+  sta $2006   ; store the most significant byte in the PPU write address (0x2006)
+  lda #$00    ; load the value 0 into the accumulator
+  sta $2006   ; store the least significant byte in the PPU write address (0x2006)
+  
+  ldx #$00    ; load the value 0 into the X register
+
+  
+  :
+  lda MAPDATA,x   ; load the value at the address stored in X and Y into the accumulator
+  sta $2007   ; store the value in the PPU data register (0x2007)
+  inx         ; increment the X register
+  cpx #$FF ; compare X with the value 4096 (the size of a nametable in bytes)
+  bne :-
+
+ ldx #$00    ; load the value 0 into the X register
+
+
+  :
+  lda MAPDATA+255,x   ; load the value at the address stored in X and Y into the accumulator
+  sta $2007   ; store the value in the PPU data register (0x2007)
+  inx         ; increment the X register
+  cpx #$FF ; compare X with the value 4096 (the size of a nametable in bytes)
+  bne :-
+   
+ ldx #$00    ; load the value 0 into the X register
+
+  :
+  lda MAPDATA+510,x   ; load the value at the address stored in X and Y into the accumulator
+  sta $2007   ; store the value in the PPU data register (0x2007)
+  inx         ; increment the X register
+  cpx #$FF ; compare X with the value 4096 (the size of a nametable in bytes)
+  bne :-
+   ldx #$00    ; load the value 0 into the X register
+
+   :
+  lda MAPDATA+765,x   ; load the value at the address stored in X and Y into the accumulator
+  sta $2007   ; store the value in the PPU data register (0x2007)
+  inx         ; increment the X register
+  cpx #$FF ; compare X with the value 4096 (the size of a nametable in bytes)
+  bne :-
+  
+
+  cli
+
+  lda #%00011110   ;enable sprites and backgrounds for left most 8 pixels
+  sta $2001
+ 
+
 rts
 
-do_action:
-       asl $1800
-       tax
-       lda TABLE+1,x
-       pha
-       lda TABLE,x
-       pha
-       rts
+
+
+test:
+  lda #$3F
+  sta $2006 ;store most significant value 3f in ppu write address 3f.. (the adress where you store the address you want to write too in the ppu)
+  lda #$00
+  sta $2006 
+  ldx $0000
+  inx
+  stx $0000
+  lda $0000
+  sta $2007 ; PPUDATA memory address to wright data to ppu (ppu puts this value in the adress defined in memory address from $2006) ppu auto increments memory address in $2006 on every wright in $2007
+  lda #$26
+  sta $2007
+  lda #$27
+  sta $2007
+  lda #$28
+  sta $2007
+  cpx #$3f
+
+rts
+
+
+
 
 CLEANPPU:
   lda #$02 ;select most significant bite
@@ -150,26 +255,30 @@ VBLANK: ;nmi or vblank what happens in the vblank
   sta $4014
   rti
 
-TABLE: ;jmptable
-  .word Loopy-1 ;
+
 
 PALLETEDATA:
-  .byte $22,$29,$1A,$0F,$22,$36,$17,$0f,$22,$30,$21,$0f,$22,$27,$17,$0F  ;background palette data
-  .byte $22,$16,$27,$18,$22,$1A,$30,$27,$22,$16,$30,$27,$22,$0F,$36,$17  ;sprite palette data
+  .byte $01,$2D,$3D,$30,$01,$36,$17,$0f,$01,$30,$21,$0f,$01,$27,$17,$0F  ;background palette data
+  .byte $22,$16,$27,$18,$22,$16,$27,$18,$22,$16,$27,$18,$22,$16,$27,$18;sprite palette data
 
 SPRITEDATA:
-  .byte $08, $00, $00, $08
-  .byte $08, $01, $00, $10
-  .byte $10, $02, $00, $08
-  .byte $10, $03, $00, $10
-  .byte $18, $04, $00, $08
-  .byte $18, $05, $00, $10
-  .byte $20, $06, $00, $08
-  .byte $20, $07, $00, $10
+  .byte $09, $9a, $02, $00
+  .byte $09, $9b, $02, $08
+  .byte $11, $aa, $02, $00
+  .byte $11, $ab, $02, $08
+  .byte $19, $9c, $07, $00
+  .byte $19, $9d, $07, $08
+  .byte $21, $ac, $07, $00
+  .byte $21, $ad, $07, $08
+
+MAPDATA:
+ .incbin "../resource/test.nam"
+
 
 .segment "VECTORS"
     .word VBLANK
     .word RESET
 
 .segment "CHARS"  
-      .incbin "../resource/hellomario.chr"
+      .incbin "../resource/elementzones.chr"
+      
--- tools/make.bat
+++ tools/make.bat
@@ -2,11 +2,11 @@ set filename=nes
 set outputname=game
 
 
-del .\bin\game.nes
+del ..\bin\game.nes
 echo %filename%
-ca65 ./code/nes.asm -o ./bin/nes.o -t nes --debug-info
-ld65 ./bin/nes.o -o ./bin/game.nes -t nes --dbgfile ./bin/game.dbg
+ca65 ../code/nes.asm -o ../bin/nes.o -t nes --debug-info
+ld65 ../bin/nes.o -o ../bin/game.nes -t nes --dbgfile ../bin/game.dbg
 
-del .\bin\nes.o
+del ..\bin\nes.o
 
 
